259b745cc8daa5ebe964c4b5450ef13500bb425c
Mateen <mateenqazi125@gmail.com>
2023-03-09 14:03:27 +0500

    Event Components

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,5 +1,31 @@
-import { useRouter } from "next/router";
-export default function EventIdPage() {
-  const route = useRouter();
-  return <div>EventIdPage</div>;
+import Layout from "@/components/Layout";
+import { API_URL } from "config";
+
+export default function EventIdPage({ evt }) {
+  return (
+    <Layout>
+      <div>{evt?.name}</div>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  const paths = events.map((evt) => ({
+    params: { id: evt.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { id } }) {
+  const res = await fetch(`${API_URL}/api/events/${id}`);
+  const events = await res.json();
+  return {
+    props: { evt: events },
+    revalidate: 1,
+  };
 }

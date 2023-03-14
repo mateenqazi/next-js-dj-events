df17e19b0f5f44af1657b37f9b29083a8517e37c
Mateen <mateenqazi125@gmail.com>
2023-03-14 13:37:09 +0500

    single page changes

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,10 +1,47 @@
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
 import Layout from "@/components/Layout";
-import { API_URL } from "config";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
 export default function EventIdPage({ evt }) {
+  const deleteEvent = (e) => {
+    console.log("delete");
+  };
   return (
     <Layout>
-      <div>{evt?.name}</div>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt?.id}`}>
+            <FaPencilAlt /> Edit Event
+          </Link>
+          <Link href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </Link>
+        </div>
+
+        <span>
+          {evt?.date} at {evt?.time}
+        </span>
+        <h1>{evt?.name}</h1>
+        {evt?.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt?.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt?.description}</p>
+        <h3>Venue: {evt?.venue}</h3>
+        <p>{evt?.address}</p>
+
+        <Link href="/events" className={styles.back}>
+          {"<"} Go Back
+        </Link>
+      </div>
     </Layout>
   );
 }
@@ -13,7 +50,7 @@ export async function getStaticPaths() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
   const paths = events.map((evt) => ({
-    params: { id: evt.slug },
+    params: { id: evt.id },
   }));
   return {
     paths,
